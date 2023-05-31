5f789b2d4713f8a765eb047b9a7af695f3606ff3
Moein Alkozay <moein.alkozay@microcis.net>
2023-05-31 21:38:57 +0430

    feat:Add the card widget and define constants

---
+++ lib/constants.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+
+
+const kButtonColor=Color(0xFFEB1555);
+const kActiveCardColor=Color(0xFF1D1E33);
+const kInactiveCardColor=Color(0xFF111328);
+
+
+const kWidgetcardstyle=TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kNumberStyle=TextStyle(
+    fontSize: 50.0,
+    fontWeight: FontWeight.w900
+);
+
+const kCalculateButton=TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleColor=TextStyle(
+    fontSize: 50.0,
+    fontWeight: FontWeight.bold
+);
+
+
+const kResultTextStyle=TextStyle(
+    color: Color(0xFF24D876),
+    fontSize: 22.0,
+    fontWeight: FontWeight.bold
+);
+
+const kBMIstyle=TextStyle(
+    fontSize: 100.0,
+    fontWeight: FontWeight.bold
+);
+
+const kBodystyle=TextStyle(
+  fontSize: 22.0,
+);
