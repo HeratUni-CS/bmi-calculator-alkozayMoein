e0bbeafd13498dd766d8a06e717f46bc971f18ea
Moein Alkozay <moein.alkozay@microcis.net>
2023-05-31 21:24:54 +0430

    feat:Get dependencies and add the input file

--- lib/input.dart
+++ lib/input.dart
@@ -1,17 +1,4 @@
 import 'package:flutter/material.dart';
-import 'inputPage.dart';
-
-void main() => runApp(BMICalculator());
-
-class BMICalculator extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark().copyWith(),
-      home: InputPage(),
-    );
-  }
-}
 
 class InputPage extends StatefulWidget {
   @override
